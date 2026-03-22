00f271efba864831e893dd41b727fbcb4adae18d
Laxus <alejandrocalzadarodriguez@gmail.com>
2026-03-22 09:39:59 -0600

    Add files via upload
    Se realiza la primera integración del proyecto

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function actualizarHora() {                     // Declara la función que actualizará la hora en pantalla
-    const reloj = document.getElementById("clock"); // Obtiene el elemento con id "clock" del DOM
+    const reloj = document.getElementById("reloj"); // Obtiene el elemento con id "reloj" del DOM
     const ahora = new Date();                      // Crea un objeto Date con la fecha y hora actuales
 
     const hh = String(ahora.getHours()).padStart(2, "0");   // Extrae la hora, la convierte a texto y agrega 0 si falta
     const mm = String(ahora.getMinutes()).padStart(2, "0"); // Extrae los minutos, convierte a texto y asegura 2 dígitos
     const ss = String(ahora.getSeconds()).padStart(2, "0"); // Extrae los segundos, convierte a texto y asegura 2 dígitos
 
-    reloj.textContent = `${hh}:${mm}:${ss}`;  // Inserta la hora formateada dentro del elemento #clock
+    reloj.textContent = `${hh}:${mm}:${ss}`;  // Inserta la hora formateada dentro del elemento #reloj
 }                                             // Fin de la función
 
 setInterval(actualizarHora, 1000);            // Ejecuta actualizarHora cada 1000 ms (1 segundo)
